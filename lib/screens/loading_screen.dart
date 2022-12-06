e42c527d4e6b3a2dddc868f22a37dbd4e0abeec8
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2022-12-06 20:42:28 +0100

    Created Location() in location.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,38 +10,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    Future<void> getCurrentLocationCheckingPermissions() async {
-      bool serviceEnabled;
-      LocationPermission locationPermission;
+    getLocation();
+  }
 
-      // Test if location services are enabled.
-      serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      if (!serviceEnabled) {
-        // Location services are not enabled don't continue
-        // accessing the position and request users of the
-        // App to enable the location services.
-        return Future.error(
-            'Location services are disabled. Please activate them.');
-      } else {
-        locationPermission = await Geolocator.checkPermission();
-        if (LocationPermission.unableToDetermine == locationPermission) {
-          return Future.error(
-              'Unable to determine if location permissions are enabled.');
-        } else if (LocationPermission.denied == locationPermission ||
-            LocationPermission.deniedForever == locationPermission) {
-          print(Future.error('Location permissions are denied: ' +
-              locationPermission.toString()));
-          locationPermission = await Geolocator.requestPermission();
-        }
-
-        if (LocationPermission.whileInUse == locationPermission ||
-            LocationPermission.always == locationPermission) {
-          Position position = await Geolocator.getCurrentPosition(
-              desiredAccuracy: LocationAccuracy.lowest);
-          print('Position: $position');
-        }
-      }
-    }
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
   }
 
   @override
